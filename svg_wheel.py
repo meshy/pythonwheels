1511c654bb27999898f25d3b137b934562523d49
Hugo <hugovk@users.noreply.github.com>
2018-03-25 11:59:42 +0300

    Cast to float not needed for Python 3

--- svg_wheel.py
+++ svg_wheel.py
@@ -82,7 +82,7 @@ def add_fraction(wheel, packages, total):
     packages_with_wheels.text = '{0}'.format(wheel_packages)
 
     title = et.SubElement(packages_with_wheels, 'title')
-    percentage = '{:.0%}'.format(wheel_packages/float(total))
+    percentage = '{:.0%}'.format(wheel_packages/total)
     title.text = percentage
 
     # Dividing line
